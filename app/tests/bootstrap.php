5337906adc240f0fdde3ed9fcc76bf256cce9d6f
DESKTOP-NQBSCE9\mcnin <mcninu23@gmail.com>
2025-10-03 14:04:13 +0200

    Install phpUnit

---
+++ app/tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
+
+if (file_exists(dirname(__DIR__).'/.env.test')) {
+    (new Dotenv())->usePutenv()->bootEnv(dirname(__DIR__).'/.env.test');
+}
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+}
